9e2d1d6afa85fd3ecae4ea9927c2f3fae050c23e
Baktiar Miah <baktiar.m@gmail.com>
2019-11-29 22:18:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,12 +16,12 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number=params[:number].to_i
     @phrase=params[:phrase]
-     def repeat(@phrase, @number)
-       print phrase * @number
+     def repeat(phrase, number)
+       print phrase * number
      end
      repeat(@phrase, @number)
    end
 
-  
+
 
 end
