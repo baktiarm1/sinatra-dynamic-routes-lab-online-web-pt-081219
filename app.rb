21f7fc1395cb1399a517944005a0d17d8db4e62f
Baktiar Miah <baktiar.m@gmail.com>
2019-11-29 22:10:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,6 +6,11 @@ class App < Sinatra::Base
   get '/reversename/:name' do
     @name=params[:name]
     @name.reverse
+  end
+
+  get '/square/:number' do
+    @number=params[:number]
+    "#{@number*@number}"
   end 
 
 end
